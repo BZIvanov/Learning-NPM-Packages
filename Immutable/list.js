6e2741a022e401de9ef6bb573f060081156c37d0
BZIvanov <ivanov.biser@gmx.com>
2021-11-03 22:04:30 +0200

    immutable count and countBy for list examples

--- Immutable/list.js
+++ Immutable/list.js
@@ -7,4 +7,15 @@ console.log('Fruits List: ', fruits);
 const filtered = fruits.filter((fruit) => fruit !== 'orange');
 console.log('Filtered fruit: ', filtered.get(1));
 
+// count and countBy
+const myObj = Immutable.fromJS([
+  { name: 'Iva', city: 'Sofia' },
+  { name: 'Tanq', city: 'Plovdiv' },
+  { name: 'Georgi', city: 'Sofia' },
+]);
+const fromSofia = myObj.count((person) => person.get('city') === 'Sofia'); // 2
+const countByCity = myObj.countBy((person) => person.get('city'));
+countByCity.get('Sofia'); // 2
+countByCity.get('Plovdiv'); // 1
+
 console.log('\n\n\n');
