c458dae64b8540b936978e6289a8da997c963e26
BZIvanov <ivanov.biser@gmx.com>
2022-05-13 10:58:12 +0300

    update object value in array immutable

--- Immutable/list.js
+++ Immutable/list.js
@@ -5,17 +5,29 @@ const fruits = List(['apple', 'lemon', 'orange']);
 console.log('Fruits List: ', fruits);
 
 const filtered = fruits.filter((fruit) => fruit !== 'orange');
-console.log('Filtered fruit: ', filtered.get(1));
+console.log('Filtered fruit: ', filtered.get(1)); // lemon
 
 // count and countBy
-const myObj = Immutable.fromJS([
+const persons = Immutable.fromJS([
   { name: 'Iva', city: 'Sofia' },
   { name: 'Tanq', city: 'Plovdiv' },
   { name: 'Georgi', city: 'Sofia' },
 ]);
-const fromSofia = myObj.count((person) => person.get('city') === 'Sofia'); // 2
-const countByCity = myObj.countBy((person) => person.get('city'));
+const fromSofia = persons.count((person) => person.get('city') === 'Sofia'); // 2
+const countByCity = persons.countBy((person) => person.get('city'));
 countByCity.get('Sofia'); // 2
 countByCity.get('Plovdiv'); // 1
+
+// update value for an object nested in an array
+const transformedData = persons.map((person) => {
+  let transformedPerson = person;
+  if (person.get('name') === 'Iva') {
+    // methods like set are always returning new value so we will assign it to a variable
+    transformedPerson = person.set('city', 'Burgas');
+  }
+  return transformedPerson;
+});
+
+console.log('Updated person city: ', transformedData.toJS());
 
 console.log('\n\n\n');
